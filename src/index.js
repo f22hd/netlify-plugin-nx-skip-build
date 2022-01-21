1c0ce58d5274b8154b6d581510f2eb9143d60aa5
fahd_allebdi <fa88hd@gmail.com>
2022-01-21 23:09:13 +0300

    fix: skipping first deployment

--- src/index.js
+++ src/index.js
@@ -1,26 +1,30 @@
-const { basename } = require('path');
+const { basename } = require('path')
 
 module.exports = {
   onPreBuild: ({ utils, constants }) => {
-      const projectName = process.env.PROJECT_NAME || basename(constants.PUBLISH_DIR);
-      const lastDeployedCommit = process.env.CACHED_COMMIT_REF
-      const latestCommit = 'HEAD'
-      const projectHasChanged = projectChanged(
-        projectName,
-        lastDeployedCommit,
-        latestCommit,
+    const projectName =
+      process.env.PROJECT_NAME || basename(constants.PUBLISH_DIR)
+    const lastDeployedCommit = process.env.CACHED_COMMIT_REF
+    const latestCommit = process.env.COMMIT_REF
+    const firstDeployment =
+      process.env.CACHED_COMMIT_REF === process.env.COMMIT_REF
+    const projectHasChanged = projectChanged(
+      projectName,
+      lastDeployedCommit,
+      latestCommit,
+    )
+
+    if (!projectHasChanged && !firstDeployment) {
+      utils.build.cancelBuild(
+        `Build was cancelled because ${projectName} was not affected by the latest changes`,
       )
-      if (!projectHasChanged) {
-        utils.build.cancelBuild(
-          `Build was cancelled because ${projectName} was not affected by the latest changes`,
-        )
-      }
+    }
   },
 }
 // eslint-disable-next-line no-unused-vars
 function projectChanged(currentProject, fromHash, toHash) {
   const execSync = require('child_process').execSync
-  const getAffected = `nx print-affected  --plain --base=${fromHash} --head=${toHash}`;
+  const getAffected = `nx print-affected  --plain --base=${fromHash} --head=${toHash}`
   const output = execSync(getAffected).toString()
   //get the list of changed projects from the output
   const changedProjects = JSON.parse(output).projects // array of affected projects
