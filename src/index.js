9a29cdb4abb1c781dff0918ac4227f8118172049
fahd_allebdi <fa88hd@gmail.com>
2022-06-03 14:51:52 +0300

    feat: forcing deployment

--- src/index.js
+++ src/index.js
@@ -3,18 +3,20 @@ const { basename } = require('path')
 module.exports = {
   onPreBuild: ({ utils, constants }) => {
     const projectName =
-      process.env.PROJECT_NAME || basename(constants.PUBLISH_DIR)
-    const lastDeployedCommit = process.env.CACHED_COMMIT_REF
-    const latestCommit = process.env.COMMIT_REF
+      process.env.PROJECT_NAME || basename(constants.PUBLISH_DIR);
+    const lastDeployedCommit = process.env.CACHED_COMMIT_REF;
+    const latestCommit = process.env.COMMIT_REF;
     const firstDeployment =
-      process.env.CACHED_COMMIT_REF === process.env.COMMIT_REF
+      process.env.CACHED_COMMIT_REF === process.env.COMMIT_REF;
+    const isForcedDeployment = process.env.NX_FORCE_DEPLOYMENT || false;
+
     const projectHasChanged = projectChanged(
       projectName,
       lastDeployedCommit,
       latestCommit,
     )
 
-    if (!projectHasChanged && !firstDeployment) {
+    if (!projectHasChanged && !firstDeployment && !isManualDeployment(isForcedDeployment)) {
       utils.build.cancelBuild(
         `Build was cancelled because ${projectName} was not affected by the latest changes`,
       )
@@ -29,4 +31,8 @@ function projectChanged(currentProject, fromHash, toHash) {
   //get the list of changed projects from the output
   const changedProjects = JSON.parse(output).projects // array of affected projects
   return changedProjects.findIndex((project) => project === currentProject) > -1
+}
+
+function isManualDeployment(isForcedDeployment){
+  return isForcedDeployment === 'true';
 }
